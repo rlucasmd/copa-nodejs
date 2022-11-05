0c3c8fde4cf237711cdd937d75991a93e8750330
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2022-11-04 23:53:11 -0300

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from 'express';
 import { loggerMiddleware } from './middleware/loggerMiddleware';
+import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+
+const prisma = new PrismaClient({
+  log: ['query']
+});
 
 const PORT = process.env.PORT ?? 3333;
 
 
 const app = express();
+
+app.use(cors({
+  origin: true
+}));
 
 app.use(loggerMiddleware);
 app.use(express.json());
@@ -14,6 +24,12 @@ app.get('/', (req: Request, res: Response) => {
   return res.json({ message: "Hello, World!" });
 });
 
-app.listen(PORT, () => {
+app.get('/pools/count', async (req: Request, res: Response) => {
+  const count = await prisma.pool.count();
+
+  return res.status(200).json({ count });
+});
+
+app.listen(PORT, /*"0.0.0.0",*/() => {
   console.log(`Server is running, listening on PORT{${PORT}}`)
 });
