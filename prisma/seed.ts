8abcbe95a5c4b44a6829f72c6404e1175f480549
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2022-11-06 00:36:23 -0300

    db: prisma - create structure / create seed

---
+++ prisma/seed.ts
@@ -0,0 +1,63 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: "John Doe",
+            email: "johndoe@email.com",
+            avatarUrl: "https://github.com/ranieri3232.png"
+        }
+    });
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example Pool',
+            code: 'Bool123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    });
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-11T12:00:00.099Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR'
+        }
+    });
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-12T12:00:00.099Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 3,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+    });
+}
+
+main();
